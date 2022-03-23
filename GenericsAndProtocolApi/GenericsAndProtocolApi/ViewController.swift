a8f82bd7682f57cbd4567b45adbe3efafa5172a2
Alphonsa <alphonsavarghese22@gmail.com>
2022-03-23 08:13:16 +0530

    initial commit

---
+++ GenericsAndProtocolApi/GenericsAndProtocolApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericsAndProtocolApi
+//
+//  Created by Alphonsa Varghese on 23/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
